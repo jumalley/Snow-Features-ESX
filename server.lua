4d3954b7e815ab6555271121c0bbee9510f06b6f
Ju <jumalleydev@gmail.com>
2024-12-23 17:50:38 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,41 +1,37 @@
---check for snow on the ground
+ESX = exports["es_extended"]:getSharedObject()
+
 lib.callback.register('checkSnowballCount', function(source)
     local snowballs = exports.ox_inventory:GetItemCount(source, 'weapon_snowball')
-    if snowballs >= 10 then
-        return false
-    end
-    return true
+    return snowballs < 10
 end)
+
 RegisterNetEvent('qbx_smallresources:server:addSnowballToInv', function()
-    exports.ox_inventory:AddItem(source, 'weapon_snowball', 1) -- Add the item back to the inventory
+    exports.ox_inventory:AddItem(source, 'weapon_snowball', 1)
 end)
-
---------snowman making-------
-
 
 RegisterNetEvent('qbx_smallresources:server:removeSnowballToInv', function()
-    exports.ox_inventory:RemoveItem(source, 'weapon_snowball', 1) -- Add the item back to the inventory
-end)
-
-exports.qbx_core:CreateUseableItem('weapon_snowball', function(source, item)
-    print('Using item')
-    TriggerClientEvent('qbx_smallresources:client:snowmanProgress', source)
+    exports.ox_inventory:RemoveItem(source, 'weapon_snowball', 1)
 end)
 
 RegisterNetEvent('qbx_smallresources:server:removeSnowballAndCarrotFromInv', function()
-    local source = source   
-    exports.ox_inventory:RemoveItem(source, 'weapon_snowball', 10) -- Remove the item from the inventory
-    exports.ox_inventory:RemoveItem(source, 'carrot', 1) -- Remove the item from the inventory
+    exports.ox_inventory:RemoveItem(source, 'weapon_snowball', 10)
+    exports.ox_inventory:RemoveItem(source, 'carrot', 1)
 end)
 
 lib.callback.register('checkSnowballAndCarrotCount', function(source)
     local snowballs = exports.ox_inventory:GetItemCount(source, 'weapon_snowball')
     local carrot = exports.ox_inventory:GetItemCount(source, 'carrot')
-    if snowballs >= 10 and carrot >= 1 then
-        return false
-    end
-    return true
+    return snowballs < 10 or carrot < 1
 end)
+
 lib.callback.register('snow:GetBucket', function(source)
     return GetPlayerRoutingBucket(source)
+end)
+
+ESX.RegisterUsableItem('weapon_snowball', function(source)
+    TriggerClientEvent('qbx_smallresources:client:snowmanProgress', source)
+end)
+
+ESX.RegisterUsableItem('carrot', function(source)
+    TriggerClientEvent('qbx_smallresources:client:snowmanProgress', source)
 end)
